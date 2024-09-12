7f5f24d27a899f50300f495d01bd31c5ca935935
TaoGe <wtao678@live.com>
2024-09-12 20:38:26 +0800

    新增收藏夹

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -38,6 +38,7 @@ export default sidebar({
       children: "structure",
     },
     "/others/",
+    "favorites",
     "intro",
   ],
   // "/": "structure"
---
+++ src/favorites.md
@@ -0,0 +1,5 @@
+---
+title: 收藏夹
+icon: star
+---
+- [通往AGI之路](https://waytoagi.feishu.cn/wiki/QPe5w5g7UisbEkkow8XcDmOpn8e)
